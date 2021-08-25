80bd40a1b9cbaca25edc58a8fd387160335b078c
Rishabh Singh <ris8645208@gmail.com>
2021-08-25 14:28:30 +0530

    Added Post list to the admin

--- client/src/components/PostList.jsx
+++ client/src/components/PostList.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
-import {List, Datagrid, TextField, DateField, EditButton, DeleteButton} from 'react-admin';
+import { List, Datagrid, TextField, DateField, EditButton, DeleteButton } from 'react-admin';
 
 const PostList = (props) => {
     return (
         <List {...props}>
             <Datagrid>
-                <TextField source='id'/>
-                <TextField source='title'/>
-                <TextField source='publishedDate'/>
-                <EditButton basePath='/posts'/>
+                <TextField source='id' />
+                <TextField source='title' />
+                <TextField source='publishedDate' />
+                <EditButton basePath='/posts' />
                 <DeleteButton basePath='/posts' />
             </Datagrid>
         </List>
